81823e33dd6bc573cd79ad02ebb8acce5be987a9
monaco07 | Leonard <80672448+monaco07@users.noreply.github.com>
2023-01-23 22:21:40 +0100

    Update main.ino
    Fuktioniert ohne zeit und ohne motor

--- main.ino
+++ main.ino
@@ -1,40 +1,78 @@
-#include <Time.h>
+int pinLDR = A1; // select the input pin for LDR
+int sensorValue = 0; // variable to store the value coming from the sensor
+
+int light_day = 600;
+// Ab der Variable kann die klappe durch den ldr wert hoch
+// mindeste für hochfahren morgen
+
+
+int night_day = 300;
+// Ab der Variable kann die klappe durch den ldr wert runter
+// mindeste für runterfahren abend
+
+bool isUnten = false;
+
+
+
+bool check_hoch(){
+  if(isUnten && sensorValue > light_day){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+bool check_runter(){
+  if(!(isUnten) && sensorValue < night_day){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+
+
+void motor_down(){
+  Serial.println("klappe hoch");
+  isUnten = !isUnten;
+}
+
+void motor_up(){
+  Serial.println("klappe runter");
+  isUnten = !isUnten;
+}
+
 void setup() {
-  // import time
-  // IMPORT MOTOREN PORTS
+  Serial.begin(9600); //sets serial port for communication
   pinMode(A1, INPUT);
-  Serial.begin(9600);
+
 }
 
 void loop() {
-  
+  sensorValue = analogRead(pinLDR); // read the value from the sensor
+  Serial.println(sensorValue);
+
+  bool can_hoch = check_hoch();
+  bool can_runter = check_runter();
+
+  switch (isUnten) {
+    case false: {
+      if (can_runter) {
+        motor_up();
+      }
+      break;
+    }
+    case true: {
+      if (can_hoch) {
+        motor_down();
+      }
+      break;
+    }
+  }
+
+
   delay(2000);
-  int ldr_value = analogRead(A1);
-  Serial.println(ldr_value);
-  bool isUnten = false;
-  int delay = 1000 * 60 * 5; // alle 5 mins
-  bool can_runter = false;
-  bool can_hoch = true;
-
-  
- switch (isUnten) {
-  case false:
-    if (can_runter) {
-      motor_down();
-    }
-    break;
-  case true:
-    if (can_hoch) {
-      motor_up();
-    }
-    break;
-}
-  
 }
 
-void motor_down(){
-  
-}
-void motor_up(){
-  
-}
