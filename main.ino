f68451f973e1e0539840406d2c418c50687e15e4
monaco07 | Leonard <80672448+monaco07@users.noreply.github.com>
2023-01-30 23:51:41 +0100

    Update main.ino
    motor + einstellungen fehlen

--- main.ino
+++ main.ino
@@ -1,78 +1,94 @@
-int pinLDR = A1; // select the input pin for LDR
-int sensorValue = 0; // variable to store the value coming from the sensor
+#include <Wire.h>
+#include <RTClib.h>
 
-int light_day = 600;
-// Ab der Variable kann die klappe durch den ldr wert hoch
-// mindeste für hochfahren morgen
+int loop_delay = 10000; // 10s zeit, die die loop pausiert
+int light_day = 600; // LDR-Wert ab dem die Klappe hochfahren kann
 
+bool isUnten = false; // ist klappe unten ?
+int sensorValue = 0; // Variable zur Speicherung des LDR-Werts
 
-int night_day = 300;
-// Ab der Variable kann die klappe durch den ldr wert runter
-// mindeste für runterfahren abend
-
-bool isUnten = false;
-
-
-
-bool check_hoch(){
-  if(isUnten && sensorValue > light_day){
+bool check_hoch() {
+  if (isUnten && sensorValue > light_day) {
     return true;
-  }
-  else{
-    return false;
-  }
-}
-
-bool check_runter(){
-  if(!(isUnten) && sensorValue < night_day){
-    return true;
-  }
-  else{
+  } 
+  else {
     return false;
   }
 }
 
 
 
-void motor_down(){
-  Serial.println("klappe hoch");
-  isUnten = !isUnten;
-}
-
-void motor_up(){
+void motor_down() {
   Serial.println("klappe runter");
-  isUnten = !isUnten;
+  isUnten = true;
 }
+
+void motor_up() {
+  Serial.println("klappe hoch");
+  isUnten = false;
+}
+
+RTC_DS3231 rtc;
+
+
+
 
 void setup() {
-  Serial.begin(9600); //sets serial port for communication
+  Serial.begin(9600);
   pinMode(A1, INPUT);
-
+  Wire.begin();
+  rtc.begin();
 }
 
+int dunkel_time(int month){
+  if(month == 1 || month == 11 || month == 12){
+    // Januar
+    return 18;
+  }
+  if(month == 2 || month == 3){
+    return 19;
+  }
+  if(month == 4){ // zeitumstellung wird nicht von der rtc unterstützt, drum egal
+    return 20;  // +1
+  }
+  if(month >= 5 && month <9){
+    return 21; // +1
+  }
+  if(month == 9){
+    return 20; // +1
+  }
+  if(month == 10){
+    return 19; // +1
+  }
+}
 void loop() {
-  sensorValue = analogRead(pinLDR); // read the value from the sensor
+
+  sensorValue = analogRead(A1); // ließt ldr value
   Serial.println(sensorValue);
 
+  // Prüfe ob Klappe hochfahren kann
   bool can_hoch = check_hoch();
-  bool can_runter = check_runter();
 
-  switch (isUnten) {
-    case false: {
-      if (can_runter) {
-        motor_up();
-      }
-      break;
+  // zeit von rtc
+  DateTime now = rtc.now();
+  Serial.println(dunkel_time(now.month()));
+  // klappe fährt um 7 hoch, wenn sie kann
+  if (now.hour() == 7) {
+    if (can_hoch) {
+      motor_up();
     }
-    case true: {
-      if (can_hoch) {
-        motor_down();
-      }
-      break;
+  }
+  // klappe fährt um 8 spätestens hoch
+  else if (now.hour() == 8 && isUnten) {
+    motor_up();
+  }
+  // klappe fährt zur gegebenen uhrzeit hoch, vom monat abhängig
+  else if (now.hour() >= dunkel_time(now.month())) {
+    if(!isUnten){
+      motor_down();
     }
   }
 
-
-  delay(2000);
+  // wartet 10s
+  delay(loop_delay);
 }
-
